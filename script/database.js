8630aeb7da91c96a9f1a3322771312d21cfb5f18
Paul Cruse III <paul.cruse@syngenta.com>
2022-12-13 21:50:12 -0600

    made it more extensible

--- script/database.js
+++ script/database.js
@@ -1,32 +1,37 @@
-const copy = require('copy-dynamodb-table').copy;
+const copyDDBTable = require('copy-dynamodb-table').copy;
 
-
-const promiseCopy = (arguments) => {
+const copyDDB = (arguments) => {
     return new Promise((resolve, reject) => {
-        copy(arguments, function (err, result) {
+        copyDDBTable(arguments, function (err, result) {
             if (err) {
                 return reject(err);
             }
             resolve(result);
-        })
-    })
-}
+        });
+    });
+};
 
 const copy = async (event) => {
     console.log(`\n===== copying ${event.source} -> ${event.destination} =====\n`);
-    await promiseCopy({
-        config: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            sessionToken: process.env.AWS_SESSION_TOKEN,
-            region: event.region
-        },
-        source: {
-            tableName: event.source,
-        },
-        destination: {
-            tableName: event.destination,
-        },
-        log: true
-    });
-}
+    const engines = ['dynamodb'];
+    if (!engines.includes(event.engine)) {
+        throw Error(`engine ${event.engine}... yet; only supported engines are ${engines.join(', ')}`);
+    }
+    if (event.engine === 'dynamodb') {
+        await copyDDB({
+            config: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID || event.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || event.AWS_SECRET_ACCESS_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN || event.AWS_SESSION_TOKEN,
+                region: event.region
+            },
+            source: {
+                tableName: event.source
+            },
+            destination: {
+                tableName: event.destination
+            },
+            log: true
+        });
+    }
+};
