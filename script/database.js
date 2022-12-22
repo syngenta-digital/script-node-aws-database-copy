69ddaf9dbc735b8b2b0e6c85896209f4bf33b337
Paul Cruse III <paul.cruse@syngenta.com>
2022-12-21 18:48:25 -0600

    why no work

--- script/database.js
+++ script/database.js
@@ -47,7 +47,7 @@ exports.copyStack = async (event) => {
             const params = {
                 FunctionName: 'v1-console-database-copy-db',
                 InvocationType: 'Event',
-                Payload: {engine: event.engine, source: tableName, destination: destination}
+                Payload: JSON.stringify({engine: event.engine, source: tableName, destination: destination})
             };
             await lambda.invoke(params).promise();
         }
